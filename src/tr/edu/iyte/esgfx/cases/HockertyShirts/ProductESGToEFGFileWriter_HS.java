8ba92de5593aeff3271ec23925df3ddbd2631e35
Dilek Öztürk <ozturkdilek93@gmail.com>
2025-11-13 19:46:18 +0100

    Add files via upload

---
+++ src/tr/edu/iyte/esgfx/cases/HockertyShirts/ProductESGToEFGFileWriter_HS.java
@@ -0,0 +1,19 @@
+package tr.edu.iyte.esgfx.cases.HockertyShirts;
+
+import tr.edu.iyte.esgfx.cases.ProductESGToEFGFileWriter;
+
+
+public class ProductESGToEFGFileWriter_HS extends CaseStudyUtilities_HS {
+
+	public static void main(String[] args) throws Exception {
+
+		coverageLength = 2;
+		CaseStudyUtilities_HS.initializeFilePaths();
+
+		ProductESGToEFGFileWriter productESGToEFGFileWriter = new ProductESGToEFGFileWriter();
+
+		productESGToEFGFileWriter.writeToEFGFile();
+
+	}
+
+}
